e44370174a58c97ae8f6d7a5b12b9fbf86f70173
testsla <1938531151@qq.com>
2017-04-14 15:52:44 +0800

    最终提交

---
+++ src/views/leaderBoard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="guanyu">
+       <img src="http://ock9k7ih4.bkt.clouddn.com/blog/images/%E5%A4%B4%E5%83%8F.jpg" alt="">
+       <div>
+        github:
+       </div>
+    </div>
+</template>
+<style>
+.guanyu{
+    width:80%;
+}
+</style>
+<script>
+</script>
